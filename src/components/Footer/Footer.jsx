b23abc6252a17db44d01ae0e6f19aee28dab3220
YOUR NAME <ahmardiha@gmail.com>
2025-04-20 17:59:02 -0400

    Project 10

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text">Developed by Jonathan Brandt</p>
+      <p className="footer__copyright"> © 2025</p>
+    </footer>
+  );
+}
+
+export default Footer;
